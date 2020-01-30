23e94c888f203640d50e998f960938cb4178a91d
Laurent Dormoy <l.dormoy@dcmn.com>
2020-01-30 23:59:06 +0100

    JS Stalker exercise

---
+++ Javascript/Introduction/120/script.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("Hello, " + userFirstName + " " + userLastName + "!");
+console.log("I see you are " + userAge + " years old...");
