ad26d1cdda2937f5f00d58cdc93664932991b3fa
Laurent Dormoy <l.dormoy@dcmn.com>
2020-02-06 03:19:01 +0100

    Javascript arrays - improved Todo list

--- Javascript/04 Arrays/list.js
+++ Javascript/04 Arrays/list.js
@@ -5,17 +5,40 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
     //handle input
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     }
     else if (input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-
-        //add to todos array
-        todos.push(newTodo);
+        addTodo();
     }
-
+    else if (input === "delete") {
+        deleteTodo();
+    }
     //ask again for new input
     input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+    console.log("*******")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("*******")
+}
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo
+    todos.splice(index, 1)
+    console.log("Deleted Todo");
+}
