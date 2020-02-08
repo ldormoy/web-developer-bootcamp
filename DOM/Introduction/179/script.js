bfcc1d015b8fd4d8ec0af9a4c8dbc75463a49306
Laurent Dormoy <l.dormoy@dcmn.com>
2020-02-08 11:37:23 +0100

    DOM introduction - select elements

---
+++ DOM/Introduction/179/script.js
@@ -0,0 +1,11 @@
+var p1 = document.getElementById("first");
+console.log(p1);
+
+var p2 = document.getElementsByClassName("special");
+console.log(p2[0]);
+
+var p3 = document.getElementsByTagName("p");
+console.log(p3[0]);
+
+var p4 = document.querySelector("#first");
+console.log(p4);
