a72717a34b0682bba693302eef11341ef80d4478
Laurent Dormoy <l.dormoy@dcmn.com>
2020-02-12 22:32:10 +0100

    jQuery Intro - Selector Exercise

---
+++ jQuery/Introduction/210 - Selector Exercise/script.js
@@ -0,0 +1,5 @@
+$("div").css("backgroundColor", "purple");
+$("div.highlight").css("width", "200px");
+$("div#third").css("border", "2px solid orange");
+//$("div:contains('Div 1')").css("color", "pink");
+$("div:first-of-type").css("color", "pink");
