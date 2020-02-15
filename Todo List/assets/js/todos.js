469157406247dbadf898e2ea54c49294c9582d6c
Laurent Dormoy <l.dormoy@dcmn.com>
2020-02-15 23:20:01 +0100

    Todo List - Core and Logic

---
+++ Todo List/assets/js/todos.js
@@ -0,0 +1,23 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        //grabbing new Todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
